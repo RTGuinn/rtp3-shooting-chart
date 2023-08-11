66cd6fd1d150dac4a26c98aa57d951bdbb669ffb
RTGuinn <>
2023-08-11 20:45:52 +0000

    Create get_data function to get data string from user

--- run.py
+++ run.py
@@ -12,8 +12,23 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('shooting_chart')
 
-shots = SHEET.worksheet('shots')
+# shots = SHEET.worksheet('shots')
 
-data = shots.get_all_values()
+# data = shots.get_all_values()
 
-print(data)
+# print(data)
+
+def get_data():
+    """
+    Get name of player and shooting numbers to input from user
+    """
+    print("Please enter name and shooting info")
+    print("Data should be name with shot's made and shot's attempted seperated by commas.")
+    print("Example: John Doe, 34, 50\n")
+    
+    data_str = input("Enter name and shots here: ")
+
+    print(f'Name and shooting info is {data_str}')
+
+
+get_data()
